7de53fa25574a4b1263d672f58bbf5b3271284e9
Andrew Boylan <andreweboylan@gmail.com>
2026-07-21 18:25:32 -0400

    Add optimized picture frame model

--- src/components/ObjectCompositeEditor.tsx
+++ src/components/ObjectCompositeEditor.tsx
@@ -54,6 +54,7 @@ const frameModels = [
   "/3d-models/frames/picture_frame_2.glb",
   "/3d-models/frames/fancy_picture_frame_01-freepoly.org.glb",
   "/3d-models/frames/picture_frame.glb",
+  "/3d-models/frames/picture_frame_2026_07_21_optimized.glb",
   "/3d-models/frames/vintage_picture_frame..glb",
   "/3d-models/frames/wooden_picture_frame_2026_05_31_optimized.glb",
   "/3d-models/frames/gold_picture_frame_2026_05_31_optimized.glb",
