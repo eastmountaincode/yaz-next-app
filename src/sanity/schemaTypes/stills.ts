98980bf05b934b83af595e8f4287e66dcfadc6da
Andrew Boylan <andreweboylan@gmail.com>
2026-08-11 14:21:41 -0400

    Clarify alternative text guidance

--- src/sanity/schemaTypes/stills.ts
+++ src/sanity/schemaTypes/stills.ts
@@ -33,7 +33,7 @@ export const stillsType = defineType({
                 defineField({
                   name: "alt",
                   title: "Alternative text",
-                  description: "Briefly describe the image for people who can’t see it.",
+                  description: "Describe the image for people using screen readers.",
                   type: "string",
                 }),
               ],
@@ -51,7 +51,7 @@ export const stillsType = defineType({
                     defineField({
                       name: "alt",
                       title: "Alternative text",
-                      description: "Briefly describe the image for people who can’t see it.",
+                      description: "Describe the image for people using screen readers.",
                       type: "string",
                     }),
                   ],
